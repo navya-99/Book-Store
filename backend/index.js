e2e95c992454a58e3ff0660277182459e63612f0
navya-99 <padiyarnavya@gmail.com>
2024-08-10 18:01:42 +0530

    Created UI for book store, added routing to create, show, edit, delete action pages

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,13 @@ app.use(express.json());
 //option 1: allow all origins with default of cors(*)
 app.use(cors());
 //option 2: allow custom origins
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-  })
-);
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
 app.get('/', (request, response) => {
   console.log(request);
