94eccd635e283a4b0b739fb8a751dfe60606cafe
navya-99 <padiyarnavya@gmail.com>
2024-08-28 20:49:26 +0530

    Created a card component to display books, added modal to show more detail about the book and added notistack alerts

---
+++ frontend/src/components/home/BooksCard.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import BookSingleCard from "./BookSingleCard";
+
+const BooksCard = ({ books }) => {
+  return (
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {books.map((item) => (
+        <BookSingleCard key={item._id} book={item} />
+      ))}
+    </div>
+  );
+};
+
+export default BooksCard;
